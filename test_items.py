b69ee85f1d21dd717042cf52f8926b92e14364db
Vladimir Marushkevich <92493007+dragunov-m@users.noreply.github.com>
2022-07-18 17:47:56 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_add_button(browser):
+def test_add_button_in_store_page(browser):
     browser.get(url)
     button_add = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
     assert button_add.is_displayed(), \
